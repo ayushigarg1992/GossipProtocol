a824ea550425bc2cbc499ae8f166496dfdbb8bfd
pooja shah <shahpoo415@gmail.com>
2017-10-03 15:40:50 -0400

    topologies

--- lib/topologies.ex
+++ lib/topologies.ex
@@ -1,25 +1,35 @@
 defmodule Topologies do
-    
-
 
     def createLine(num) do
-        pids = Enum.map(1..num, fn(x) ->Chat.Supervisor.start_node(x)end )
+        pids = Enum.map(1..num, fn(x) ->Manager.start_node(x)end )
         list=Enum.each(pids, fn(x)->IO.inspect(x)end)
     end
 
     def createGrid(num) do
         root = :math.sqrt(num)
-        if root*root<num do
-            root = root+1
-
-        end
+        root = Float.ceil(root)
+        {root,_} = Integer.parse("#{root}")
+        IO.puts(root)
+        pids = Enum.map(1..root, fn(x) ->Manager.start_node(x) 
+                Enum.map(1..root, fn(x) ->Manager.start_node(x)end)end)
+                list=Enum.each(pids, fn(x)->IO.inspect(x)end)
+  
     end
 
     def createImpGrid(num) do
+        root = :math.sqrt(num)
+        root = Float.ceil(root)
+        {root,_} = Integer.parse("#{root}")
+        IO.puts(root)
+        pids = Enum.map(1..root, fn(x) ->Manager.start_node(x) 
+                Enum.map(1..root, fn(x) ->Manager.start_node(x)end)end)
+                list=Enum.each(pids, fn(x)->IO.inspect(x)end)
         
     end
 
     def createFull(num) do
-        
+        pids = Enum.map(1..num, fn(x) ->Manager.start_node(x)end )
+        list=Enum.each(pids, fn(x)->IO.inspect(x)end)
+        neighbor = Enum.all(pids)
     end
 end
