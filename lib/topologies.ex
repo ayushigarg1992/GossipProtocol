441bfaf2bb7b259335b2946e3a1fe4609cfc5c73
ayushigarg1992 <garg.bulbul@gmail.com>
2017-10-04 11:39:23 -0400

    grid

--- lib/topologies.ex
+++ lib/topologies.ex
@@ -1,29 +1,37 @@
 defmodule Topologies do
 
     def createLine(num) do
-        neighbor = []
-        pids = Enum.map(1..num, fn(x) ->Manager.start_node(x)end )
+        neighbors = []
+        pids = Enum.map(1..num, fn(x) ->Manager.start_node(:x)end )
         list=Enum.each(pids, fn(x)->IO.inspect(x)end)
         for n <- 0..num-1 do
             if (n==0) do
                 {_,pid} = Enum.at(pids,1)
-                neighbor = [pid|neighbor]
-            
+                neighbors = [pid|neighbors]
+                
             else if (n==num-1) do
                 {_,pid} = Enum.at(pids,n-1)
-                neighbor = [pid|neighbor]
-            
+                neighbors = [pid|neighbors]
+                
+                
             else 
                 {_,pid1} = Enum.at(pids,n+1)
                 {_,pid} = Enum.at(pids,n-1)
-                neighbor = [pid,pid1|neighbor]
-            
+                neighbors = [pid,pid1|neighbors]
+               
+                
             end
+            #to-do in the morning. Figure out message sending
+            Enum.each(list2,  fn neigh-> Server.send_rumor(neigh,n,"gossip")end)
             end
         end
+        start_transmit(neighbors,pids,num)
         
     end
 
+    def start_transmit(neighbors,pids,num) do
+        
+    end
     def createGrid(num) do
         root = :math.sqrt(num)
         root = Float.ceil(root)
